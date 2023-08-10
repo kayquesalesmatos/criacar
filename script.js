374b2f5007e1525e7674d643986153419842611c
kayquesalesmatos <141851130+kayquesalesmatos@users.noreply.github.com>
2023-08-10 10:46:53 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,35 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var slides = document.getElementsByClassName("slide");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 2000); // Troca a imagem a cada 2 segundos (ajuste conforme necessário)
+}
+
+function changeSlide(n) {
+  slideIndex += n;
+  var slides = document.getElementsByClassName("slide");
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  } else if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
